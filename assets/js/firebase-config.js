ff57ead9679403378fcc88069f9f4e1c51d7b7a6
aminur-syd <sydneyaminur@gmail.com>
2026-02-14 11:33:32 +0600

    updated auth key

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -7,7 +7,7 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-s
 // Your web app's Firebase configuration
 // TODO: Replace with your actual Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDWt4CTYOxfgx3K72c4pfeFm7q6rzLClZg",
+    apiKey: "AIzaSyDWt4CTYOxfgx3K72c4pfeFm7q6rzLC1Zg",
     authDomain: "lost-and-found-16023.firebaseapp.com",
     projectId: "lost-and-found-16023",
     storageBucket: "lost-and-found-16023.firebasestorage.app",
